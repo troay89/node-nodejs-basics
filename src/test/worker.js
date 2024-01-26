a6472b8a74dda24ff4239d6b64c68788cbeda765
Home <picccc89@gmail.com>
2024-01-26 23:15:58 +0300

    feat: ыщÐt: solution wt task

---
+++ src/test/worker.js
@@ -0,0 +1,13 @@
+import { parentPort } from 'node:worker_threads';
+
+const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+const sendResult = () => {
+    parentPort.on('message', (value) => {
+        parentPort.postMessage(nthFibonacci(value))
+    })
+}
+
+sendResult()
+
+
